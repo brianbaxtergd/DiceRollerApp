c3f4620efcdeb4978c16d4ff899d15373c32701b
Baxo <brianbaxtergd@gmail.com>
2025-02-12 19:33:55 -0600

    Initial Commit

---
+++ DiceRollerApp/DiceRollerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerAppApp.swift
+//  DiceRollerApp
+//
+//  Created by Brian Baxter on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
